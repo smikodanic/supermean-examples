b235db3b1d97ddea81ceab1b001f8f167246494a
smikodanic <smikodanic@gmail.com>
2016-08-01 17:17:47 +0200

    prod db config

--- server/api/README.md
+++
@@ -1,3 +0,0 @@
-#API - Application Programming Interface  
-This directory contains API files.  
-No need for /views/ here because the output is in JSON format (res.json()).
--- server/app/config/env/prod.js
+++ server/app/config/env/prod.js
@@ -13,10 +13,21 @@ var config = {
     },
     gulpFile: 'gulpfile-nodemon.js',
     database: {
-        mongodb: {
-            isActive: true, //true || false (if false then website will not use mongodb)
-            uri: process.env.MONGODB_URI || 'mongodb://supermean_user:smPass@127.0.0.1:27017/supermean
-        }
+        mongodb: [ //first in the list is default server and will be started on nodejs startup
+            {
+                name: 'supermean',
+                isActive: true, //true || false (if false then app will not use this mongodb)
+                uri: process.env.MONGODB_URI_1 || 'mongodb://supermean_user:smPass@127.0.0.1:27017/supermean',
+                driver: 'mongoose'
+            },
+            {
+                name: 'supermean2',
+                isActive: true,
+                uri: process.env.MONGODB_URI_2 || 'mongodb://supermean_user:somePass@127.0.0.1:27017/supermean2',
+                driver: 'mongoose'
+            }
+        ]
+
     }
 
 };
